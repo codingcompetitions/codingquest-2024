b336c29acb1a2d505cdf633320638154b4585f74
Manuel Alejandro Gómez Nicasio <az-dev@outlook.com>
2025-02-03 15:38:47 -0600

    Fix tests

--- test/codingquest/day01_test.rb
+++ test/codingquest/day01_test.rb
@@ -6,7 +6,11 @@
 require "test_helper"
 
 class Day01Test < ActiveSupport::TestCase
-    test "Day01" do
-        assert_equal 191_274, Day01.part01("../codingquest-data/2024/day01-01.in", false)
+    test "Day01 Sample" do
+        assert_equal 2533, Day01.part01("../codingquest-data/2024/sample/day01-01.in", false)
+    end
+
+    test "Day01 Input" do
+        assert_equal 191_274, Day01.part01("../codingquest-data/2024/input/day01-01.in", false)
     end
 end
