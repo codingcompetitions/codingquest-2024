1460a34c0d8bc032d2b17857fb15fe4fba8bc039
Alejandro Gómez <ceochronos@users.noreply.github.com>
2025-02-03 11:24:22 -0600

    Develop (#5)
    * Fix linting errors
    
    * Add schema.rb
    
    * Add copyright headers

--- app/cq/Day01.rb
+++ app/cq/Day01.rb
@@ -1,5 +1,10 @@
+# (c) Manuel Alejandro Gómez Nicasio <az-dev@outlook.com>
+#
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+
 class Day01
-    def self.part01 path, debug
+    def self.part01(path, debug)
         "run day01!"
     end
 end
