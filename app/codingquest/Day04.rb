5ae0c82610b0dc6af99c2106bdedc1b7ebfb22c1
Manuel Alejandro Gómez Nicasio <az-dev@outlook.com>
2025-02-14 22:24:48 -0600

    move requires around

--- app/codingquest/Day04.rb
+++ app/codingquest/Day04.rb
@@ -1,11 +1,11 @@
-# typed: strict
-
-require "sorbet-runtime"
-
 # (c) Manuel Alejandro Gómez Nicasio <az-dev@outlook.com>
 #
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
+
+# typed: strict
+
+require "sorbet-runtime"
 
 #
 #
